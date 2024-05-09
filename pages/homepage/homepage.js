2a13569e2aed68e8d056a5f9ca82fdf73bf328b6
lucasdaniellopes <lucasdanielspead@gmail.com>
2024-05-09 16:03:39 -0300

    add modal to homepage

---
+++ pages/homepage/homepage.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById("modal");
+const btn = document.getElementById("menuBtn");
+const closeBtn = document.getElementsByClassName("modal-close");
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
